0486b07be162e1afaff04c3c4f32cd25919aa843
romandatckov <romandatckov@mail.ru>
2023-11-06 17:40:07 +0300

    Задание на списки 1 №3

---
+++ src/completed/lists1/task3/Group.java
@@ -0,0 +1,56 @@
+package completed.lists1.task3;
+
+public class Group {
+    private Student student;
+
+    public Group() {
+        this.student = new Student("", "", 0, null);
+    }
+    Student getFirst(){
+        return student;
+    }
+    Student getLast(){
+        Student st = this.student;
+        while (st.getNext()!=student && st.getNext()!=null)
+            st = st.getNext();
+        return st;
+    }
+    public void insert(String name, String group, int age){
+        if (this.getLast().getNext()==null) {
+            this.getLast().setNext(new Student(name, group, age, this.student.getNext()));
+        }
+        else{
+            this.getLast().setNext(new Student(name, group, age, this.student.getNext()));
+        }
+    }
+
+    public void deleteLast(){
+        Student st = student;
+        while (st.getNext().getNext()!=null){
+            st = st.getNext();
+        }
+        st.setNext(null);
+    }
+    public boolean isEmpty(){
+        return student.getNext() == null;
+    }
+    public void clear(){
+        student.setNext(null);
+    }
+
+    @Override
+    public String toString() {
+        if (student.getNext() == null){
+            return "В списке нет студентов";
+        }
+        else {
+            Student st = student.getNext();
+            String str = "";
+            do {
+                str = str + st + "\n";
+                st = st.getNext();
+            } while (st != student.getNext() && st!=null);
+            return str;
+        }
+    }
+}
